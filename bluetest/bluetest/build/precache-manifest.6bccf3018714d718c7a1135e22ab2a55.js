0a670e0e06df3db3caddf90536d2f83f8d16bd14
HYEOK JU JANG <entaline@naver.com>
2020-02-05 16:39:29 +0900

    add build

---
+++ bluetest/bluetest/build/precache-manifest.6bccf3018714d718c7a1135e22ab2a55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03246e909fbda44539c1ddcd3936b08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a41c18953dd6155b97ec",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2bdc01559c7884d66b0",
+    "url": "/static/js/2.c24d971e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c24d971e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41c18953dd6155b97ec",
+    "url": "/static/js/main.d3d7954c.chunk.js"
+  },
+  {
+    "revision": "84e1d2a02ee23bff7e57",
+    "url": "/static/js/runtime-main.cad31aca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
