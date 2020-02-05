41546bb5c609d1accc9c92b23cb598a8b8e55e20
HYEOK JU JANG <entaline@naver.com>
2020-02-05 16:43:30 +0900

    build

---
+++ bluetest/bluetest/build/precache-manifest.739f20746326b3e2f65179d7209fe4de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1fa2ffd47056aec219ab7dbd9f15ee",
+    "url": "/bluetest/bluetest/build/index.html"
+  },
+  {
+    "revision": "c4873eed286e91c28523",
+    "url": "/bluetest/bluetest/build/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2bdc01559c7884d66b0",
+    "url": "/bluetest/bluetest/build/static/js/2.c24d971e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bluetest/bluetest/build/static/js/2.c24d971e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4873eed286e91c28523",
+    "url": "/bluetest/bluetest/build/static/js/main.b1f7b985.chunk.js"
+  },
+  {
+    "revision": "2dda9b5e983e0f47dd90",
+    "url": "/bluetest/bluetest/build/static/js/runtime-main.1cb5ae09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bluetest/bluetest/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
