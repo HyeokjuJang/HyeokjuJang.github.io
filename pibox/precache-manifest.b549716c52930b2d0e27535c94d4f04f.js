9da921593781fbdbeae676660058fd38310f949e
HYEOK JU JANG <entaline@naver.com>
2020-02-22 13:38:06 +0900

    add pibox

---
+++ pibox/precache-manifest.b549716c52930b2d0e27535c94d4f04f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e11f1c567ac0d07e785fd9a1bb06b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c48f01efffd6453f4d28",
+    "url": "/static/css/main.027be71b.chunk.css"
+  },
+  {
+    "revision": "200a0ecab24d36e1b7f7",
+    "url": "/static/js/2.389f080e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.389f080e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48f01efffd6453f4d28",
+    "url": "/static/js/main.53be593b.chunk.js"
+  },
+  {
+    "revision": "02188ae2cdf142e1041b",
+    "url": "/static/js/runtime-main.627fd951.js"
+  }
+]);
