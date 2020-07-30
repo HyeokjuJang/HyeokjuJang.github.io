425ddb00d3deed18cfe9b6a747e0f34175942613
HYEOK JU JANG <entaline@naver.com>
2020-07-31 02:32:02 +0900

    pibox fixed

---
+++ pibox/precache-manifest.b524c608bce42727c65bfd595d20e357.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c83c48ff215f6fd23c1ab3f1401bec4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "774cbe7f2e4f07fb8ab5",
+    "url": "./static/css/main.37a9773e.chunk.css"
+  },
+  {
+    "revision": "a31e5a16cd8c082e9c7f",
+    "url": "./static/js/2.15118dfd.chunk.js"
+  },
+  {
+    "revision": "a77b9cab74a4a72f39afdc9bf36521e7",
+    "url": "./static/js/2.15118dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774cbe7f2e4f07fb8ab5",
+    "url": "./static/js/main.d658ff27.chunk.js"
+  },
+  {
+    "revision": "e66ae0cb2887af5aefe4",
+    "url": "./static/js/runtime-main.adfb2b5d.js"
+  },
+  {
+    "revision": "cd049886ba7d5b3edd26ea32c6db1b3a",
+    "url": "./static/media/led.cd049886.png"
+  },
+  {
+    "revision": "d4643de8aed0e7f5930402a539f887bd",
+    "url": "./static/media/motor.d4643de8.png"
+  },
+  {
+    "revision": "bd980005982ebcdb3daf8f26d1f69447",
+    "url": "./static/media/none.bd980005.png"
+  },
+  {
+    "revision": "ba097c0f65e69062ce6aac1ddbdfebe9",
+    "url": "./static/media/servomotor.ba097c0f.png"
+  }
+]);
