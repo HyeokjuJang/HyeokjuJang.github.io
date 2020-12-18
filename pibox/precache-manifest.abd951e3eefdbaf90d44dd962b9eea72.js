8070dac93eb0eb570638afd3912d439e4beb1726
HYEOK JU JANG <entaline@naver.com>
2020-12-18 15:51:29 +0900

    pibox updated

---
+++ pibox/precache-manifest.abd951e3eefdbaf90d44dd962b9eea72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31798e1b6af30ad03b68c824e7c9f5bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a9bf87dc3d8c214bf49",
+    "url": "./static/css/main.8f8e846f.chunk.css"
+  },
+  {
+    "revision": "5e767e0bae1cc5aee3ec",
+    "url": "./static/js/2.0028d57e.chunk.js"
+  },
+  {
+    "revision": "a77b9cab74a4a72f39afdc9bf36521e7",
+    "url": "./static/js/2.0028d57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a9bf87dc3d8c214bf49",
+    "url": "./static/js/main.de1f07b1.chunk.js"
+  },
+  {
+    "revision": "e66ae0cb2887af5aefe4",
+    "url": "./static/js/runtime-main.adfb2b5d.js"
+  }
+]);
